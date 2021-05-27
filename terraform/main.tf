ab7ed083eea93769a89e51c45205c5e4d2fac0a5
Pontus Lundgren (ndf104) <pontus.lundgren@tv4.se>
2021-05-27 19:55:34 +0200

    IGLOO-0: version bump

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 3.42"
     }
   }
 
-  required_version = ">=0.14.9"
+  required_version = ">=0.15.4"
 }
 
 provider "aws" {
